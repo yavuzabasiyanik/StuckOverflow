b04f73dbbd25416114fd16a80ed811817acc3849
Matthew <mattic11@yahoo.com>
2022-02-14 17:42:39 -0500

    update auth, login form on index/view

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require("../db/models");
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'a/A Express Skeleton Home' });
