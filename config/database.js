5e7142451a8f09decb5e093fe0aaa872abc75407
YavuzAbasiyanik <abasiyanikyavuz@gmail.com>
2022-02-15 11:31:03 -0600

    asdd

--- config/database.js
+++ config/database.js
@@ -11,4 +11,15 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: { // << THIS OBJECT
+      ssl: { // << WITH THIS NESTED OBJECT
+        require: true, // <<vv AND THESE KEY:VALUE PAIRS
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
